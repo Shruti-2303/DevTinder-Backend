efce3ab6e39478aad56228507365fb4f9ba80836
Shruti Sharma <shruti.sharma@Leverages-MacBook-Pro.local>
2025-02-06 13:49:34 +0530

    request handlers added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 
-//request handler
-app.use((req,res) => {
-    res.send("Hello from our first server");
+//for test route
+app.use("/test",(req,res) => {
+    res.send("Hello testing from our first server");
 })
-
+//for hello route
+app.use("/hello",(req,res) => {
+    res.send("Hello Hello Hello from our first server");
+})
+//request handler
+app.use("/",(req,res) => {
+    res.send("Namaste from our first server");
+})
 //listen to port
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
