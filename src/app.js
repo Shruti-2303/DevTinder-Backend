78f0907d94f65261f3f9c1e6f686cab64d329a52
Shruti Sharma <shruti.sharma@Leverages-MacBook-Pro.local>
2025-02-06 13:35:13 +0530

    added a comment

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
+//request handler
 app.use((req,res) => {
     res.send("Hello from our first server");
 })
 
+//listen to port
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
